7f6fb2e0610881ce36da44d9bd1b3c8fe4be30ce
Victor Contreras <contreras3991@gmail.com>
2025-07-13 21:54:15 -0300

    feat: create s3 bucket with boto3 script

---
+++ src/create_s3_bucket.py
@@ -0,0 +1,19 @@
+from utils.aws_client import s3_client
+from loguru import logger
+
+
+def create_s3_bucket(bucket_name: str = 'aws-sqs-to-s3-etl-bucket') -> str:
+    try:
+        s3_client.create_bucket(
+            Bucket=bucket_name
+        )
+        logger.info(f'Bucket {bucket_name} created successfully')
+
+        return bucket_name
+    except Exception as e:
+        logger.error(f'Error creating bucket: {e}')
+
+
+if __name__ == '__main__':
+    bucket_name = create_s3_bucket()
+    print(bucket_name)
