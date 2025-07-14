09f2e9d195ff176ba74d72a6da2b9749bc6e11f3
Victor Contreras <contreras3991@gmail.com>
2025-07-13 21:54:34 -0300

    feat: add genereta fake users data

--- src/generate_users.py
+++ src/generate_users.py
@@ -1,13 +1,15 @@
 from faker import Faker
+import json
 
 fake = Faker()
 
-def create_user() -> dict:
-    return {
+def create_user() -> str:
+    user = {
         'first_name': fake.first_name(),
         'last_name': fake.last_name(),
         'email': fake.email(),
         'created_at': fake.date()
     }
 
+    return json.dumps(user)
 
